82e9853b5dc7ea4b38b754691b4b26ffad318c12
ikromshi <numanovikrom454@gmail.com>
2022-01-20 00:48:12 -0500

    using classes instead of constructor functions

--- person.js
+++ person.js
@@ -1,24 +1,30 @@
-const Person = function(firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-    this.getAge = function() {return `Age is ${this.age}`}
-}
-
-// Prototypal Inheritance
-Person.prototype.getInfo = function() {
-    let bio = `${this.firstName} is ${this.age}.`
+// USING CLASSES ---------------------------------------------------------------------
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+        this.getAge = function() {return `Age is ${this.age}`}      
+    } 
+    getInfo() {
+        let bio = `${this.firstName} is ${this.age}.`
     
-    this.likes.forEach(like => {bio += ` ${this.firstName} likes ${like}.`})
-    return bio
+        this.likes.forEach(like => {bio += ` ${this.firstName} likes ${like}.`})
+        return bio 
+    }
+    setName(fullName) {
+        const names = fullName.split(" ")
+        this.firstName = names[0]
+        this.lastName = names[1] 
+    }
 }
+ 
+const meo = new Person("Ikrom", "Numonov", 18, ["Teaching"])
+console.log(meo.getInfo())
 
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(" ")
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
+// -----------------------------------------------------------------------------------
+
 
 
 const me = new Person("Ikrom", "Numonov", 18, ["Teaching", "Biking"])
